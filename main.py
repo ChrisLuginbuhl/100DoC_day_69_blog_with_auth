81ed7683d8957937da2f5c19f76cebe3beff0370
ChrisLuginbuhl <chris.luginbuhl@gmail.com>
2022-10-23 21:12:22 -0700

    Initialize database

--- main.py
+++ main.py
@@ -298,7 +298,7 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-# initialize_db()
+initialize_db()
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5001)
 
