8b7868a804852dcff043fda00bf71efdfb083a03
ChrisLuginbuhl <chris.luginbuhl@gmail.com>
2022-10-23 21:18:52 -0700

    Initialize database

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
     # Create Foreign Key, "users.id" the users refers to the tablename of User. Note this should have been an Int but too late now.
-    author_id = db.Column(db.Intege, db.ForeignKey('users.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     # Create reference to the User object, the "posts" refers to the posts property in the User class.
     author = relationship("User", back_populates='posts')
     title = db.Column(db.String(250), unique=True, nullable=False)
